1430434d6ab7930932e46849f006ebea6426e768
ltrel <79512656+ltrel@users.noreply.github.com>
2021-07-28 09:56:14 +1000

    Display list of all students on students screen

--- students_form.py
+++ students_form.py
@@ -1,7 +1,9 @@
+import requests
+import json
 from PySide6.QtWidgets import QWidget, QVBoxLayout
 from ui.students_ui import Ui_StudentsForm
 from teacher_sidebar import TeacherSidebar
-
+from utils import app_state_ref
 
 class StudentsForm(QWidget):
     def __init__(self, parent):
@@ -13,3 +15,16 @@ class StudentsForm(QWidget):
         self.ui.sidebar = TeacherSidebar(self)
         self.ui.sidebar_layout.addWidget(self.ui.sidebar)
         self.ui.sidebar.ui.students_button.setStyleSheet('background-color: blue')
+        self.students = []
+
+    def showEvent(self, event):
+        # Get a list of students from the server.
+        app_state = app_state_ref(self)
+        students_url = app_state.get_api_url('/students')
+        res = requests.get(students_url, params={'auth_token': app_state.get_token()})
+        self.students = json.loads(res.content)
+        # Populate the listview with the students.
+        for student in self.students:
+            item_string = f'{student["fullName"]} ({student["username"]})'
+            self.ui.students_listwidget.addItem(item_string)
+        print(res)
