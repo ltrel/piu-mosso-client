89a7e2d1995c2daba55bf6e0857a1d89f3034f38
ltrel <79512656+ltrel@users.noreply.github.com>
2021-07-31 19:18:21 +1000

    Allow removing students

--- students_form.py
+++ students_form.py
@@ -14,6 +14,7 @@ class StudentsForm(QWidget):
         self.ui.setupUi(self)
 
         self.ui.add_button.clicked.connect(self.add_student)
+        self.ui.remove_button.clicked.connect(self.remove_student)
 
         self.ui.sidebar = TeacherSidebar(self)
         self.ui.sidebar_layout.addWidget(self.ui.sidebar)
@@ -58,8 +59,32 @@ class StudentsForm(QWidget):
         if res.status_code != 200:
             raise Exception('API Request Failed')
 
+
         # Add the student to the teacher's students listwidget if needed.
         item_string = self.ui.students_listwidget.item(listwidget_index).text()
         item_exists = self.ui.teacher_students_listwidget.findItems(item_string, Qt.MatchExactly)
         if not item_exists:
             self.ui.teacher_students_listwidget.addItem(item_string)
+        # Add the student's details to the local list of teacher's students
+        self.teacher_students.append(self.all_students[listwidget_index])
+
+    def remove_student(self):
+        # Get the currently selected student's internal id.
+        listwidget_index = self.ui.teacher_students_listwidget.currentRow()
+        if listwidget_index == -1: return
+        student_id = self.teacher_students[listwidget_index]['studentId']
+        
+        # Tell the server to remove the student from the teacher.
+        app_state = app_state_ref(self)
+        add_student_url = app_state.get_api_url('/teacher-students')
+        res = requests.delete(add_student_url, params={'auth_token': app_state.get_token()}, json={
+            'studentId': student_id
+        })
+        # Make sure the request succeeded.
+        if res.status_code != 200:
+            raise Exception('API Request Failed')
+
+        # Remove the student from the teacher's students listwidget.
+        self.ui.teacher_students_listwidget.takeItem(listwidget_index)
+        # Remove the student from the internal list of teacher's students.
+        self.teacher_students.pop(listwidget_index)
