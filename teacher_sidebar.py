0b2d12d12d38dc3fb094b77ecd47c6c30c8c0d70
ltrel <79512656+ltrel@users.noreply.github.com>
2021-08-03 08:30:54 +1000

    Allow changing between pages using sidebar

--- teacher_sidebar.py
+++ teacher_sidebar.py
@@ -10,7 +10,32 @@ class TeacherSidebar(QWidget):
         self.ui = Ui_TeacherSidebar()
         self.ui.setupUi(self)
 
+        self.ui.new_button.clicked.connect(self.buttons_handler)
+        self.ui.last_button.clicked.connect(self.buttons_handler)
+        self.ui.calendar_button.clicked.connect(self.buttons_handler)
+        self.ui.students_button.clicked.connect(self.buttons_handler)
+        self.ui.files_button.clicked.connect(self.buttons_handler)
+        
         self.ui.sign_out_button.clicked.connect(self.sign_out)
+
+    def buttons_handler(self):
+        stacked_widget = self.window().ui.stacked_widget
+        sender = self.sender()
+        button_name = sender.objectName()
+        # Check which button was pressed and go to the relevant page
+        if button_name == 'new_button':
+            stacked_widget.setCurrentWidget(stacked_widget.new_lesson_form)
+        elif button_name == 'last_button':
+            pass
+        elif button_name == 'calendar_button':
+            pass
+        elif button_name == 'students_button':
+            stacked_widget.setCurrentWidget(stacked_widget.students_form)
+        elif button_name == 'files_button':
+            pass
+        else:
+            raise Exception('Unexpected button pressed')
+        
 
     def sign_out(self):
         stacked_widget = self.window().ui.stacked_widget
