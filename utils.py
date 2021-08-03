cd36967092cf1adba5d5a39d4830527d90486223
ltrel <79512656+ltrel@users.noreply.github.com>
2021-08-03 16:47:37 +1000

    Add title case function

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import re
 from PySide6.QtWidgets import QMessageBox
 
 
@@ -9,3 +10,8 @@ def show_message_box(title, text):
     message_box.setWindowTitle(title)
     message_box.setText(text)
     message_box.exec()
+
+def title_case(string):
+    pattern = r"[A-Za-z]+('[A-Za-z]+)?"
+    replacement = lambda match_obj: match_obj.group(0).capitalize()
+    return re.sub(pattern, replacement, string)
