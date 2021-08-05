1a26fede6ea27eb048128ec4e732d8499f74c431
ltrel <79512656+ltrel@users.noreply.github.com>
2021-08-05 17:19:19 +1000

    Show lesson details on last lesson page

--- last_lesson_form.py
+++ last_lesson_form.py
@@ -1,8 +1,9 @@
+import json
 from PySide6.QtWidgets import QWidget
 from ui.last_lesson_ui import Ui_LastLessonForm
 from lesson_info_form import LessonInfoForm
 from teacher_sidebar import TeacherSidebar
-from utils import app_state_ref
+from utils import app_state_ref, time_ms
 
 
 class LastLessonForm(QWidget):
@@ -15,7 +16,22 @@ class LastLessonForm(QWidget):
         self.ui.sidebar = TeacherSidebar(self)
         self.ui.sidebar_layout.addWidget(self.ui.sidebar)
         self.ui.sidebar.ui.last_button.setStyleSheet('background-color: blue')
+
+        self.ui.lesson_info = None
     
     def setup(self):
-        self.ui.lesson_info = LessonInfoForm(self)
+        # If there is already a LessonInfoForm here, get rid of it
+        if self.ui.lesson_info != None:
+            self.ui.lesson_info_layout.removeWidget(self.ui.lesson_info)
+
+        # Get lessons from the database
+        app_state = app_state_ref(self)
+        lessons = json.loads(app_state.api_get('/lessons').content)
+        # Filter out all lessons that haven't occurred yet
+        past_lessons = list(filter(lambda x: x['dateTime'] < time_ms(), lessons))
+        # Sort the past lessons so that the most recent is first in the list
+        past_lessons.sort(key=lambda x: x['dateTime'], reverse=True)
+
+        # Create the lesson info form using the last lesson
+        self.ui.lesson_info = LessonInfoForm(self, past_lessons[0])
         self.ui.lesson_info_layout.addWidget(self.ui.lesson_info)
--- lesson_info_form.py
+++ lesson_info_form.py
@@ -1,11 +1,24 @@
+from datetime import datetime
 from PySide6.QtWidgets import QWidget
 from ui.lesson_info_ui import Ui_LessonInfoForm
-from utils import app_state_ref
+from utils import app_state_ref, title_case
 
 
 class LessonInfoForm(QWidget):
-    def __init__(self, parent):
+    def __init__(self, parent, lesson_json=None):
         super().__init__(parent)
 
         self.ui = Ui_LessonInfoForm()
         self.ui.setupUi(self)
+
+        # Fill in the lesson details
+        self.ui.student_label.setText(f'Student: {lesson_json["studentName"]}')
+        self.ui.duration_label.setText(
+            f'Duration: {lesson_json["minutes"]} minutes')
+        self.ui.location_label.setText(f'Location: {lesson_json["location"]}')
+        self.ui.instrument_label.setText(
+            f'Instrument: {title_case(lesson_json["instrument"])}')
+
+        dt = datetime.fromtimestamp(lesson_json['dateTime'] // 1000)
+        dt_string = f'{dt:%A} {dt:%B} {dt.day}, {dt.year} {dt:%I:%M %p}'
+        self.ui.date_label.setText(f'Date: {dt_string}')
