0730b222fd5734be35b97b9849d76a1b413b7d75
ltrel <79512656+ltrel@users.noreply.github.com>
2021-08-08 18:02:59 +1000

    Make default window size bigger

--- ui/mainwindow_ui.py
+++ ui/mainwindow_ui.py
@@ -17,7 +17,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1000, 600)
+        MainWindow.resize(1200, 720)
         sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
