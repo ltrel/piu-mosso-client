30aef1686da779ee1b372708dd3a46b255b45de0
ltrel <79512656+ltrel@users.noreply.github.com>
2021-08-09 12:34:44 +1000

    Make calendar page functional

--- calendar_form.py
+++ calendar_form.py
@@ -1,6 +1,9 @@
+import json
 from PySide6.QtWidgets import QWidget
 from ui.calendar_ui import Ui_CalendarForm
 from teacher_sidebar import TeacherSidebar
+from lesson_info_form import LessonInfoForm
+from datetime import datetime, date, timedelta
 from utils import app_state_ref
 
 
@@ -13,8 +16,111 @@ class CalendarForm(QWidget):
 
         self.ui.sidebar = TeacherSidebar(self, 'calendar_button')
         self.ui.sidebar_layout.addWidget(self.ui.sidebar)
-        self.lessons = []
+
+        self.ui.next_button.clicked.connect(self.next_week)
+        self.ui.previous_button.clicked.connect(self.previous_week)
+
+        # Organize all the list widgets into a list so they can be accessed
+        # by weekday number
+        self.ui.list_widgets = [
+            self.ui.monday_list,
+            self.ui.tuesday_list,
+            self.ui.wednesday_list,
+            self.ui.thursday_list,
+            self.ui.friday_list,
+            self.ui.saturday_list,
+            self.ui.sunday_list,
+        ]
+        for list_widget in self.ui.list_widgets:
+            list_widget.itemDoubleClicked.connect(self.show_details)
+
+        self.all_lessons = []
+        # List of 7 lists where each list corresponds to a weekday
+        self.shown_lessons = [[] for i in range(7)]
+
+        self.week_monday = None
 
     def setup(self):
+        # Use most recent monday as the start of the currently shown week
+        today = datetime.combine(date.today(), datetime.min.time())
+        self.week_monday = today - timedelta(days=today.weekday())
+
+        # Label the date of each column
+        self.label_columns()
+
+        # Get lessons from the server and sort them by date
         app_state = app_state_ref(self)
-        pass
+        self.all_lessons = json.loads(app_state.api_get('/lessons').content)
+        self.all_lessons.sort(key=lambda x: x['dateTime'])
+        # Draw all of the current week's lessons to the list widgets
+        self.populate()
+        # Make sure the calendar page of the stacked widget is showing
+        self.ui.stacked_widget.setCurrentWidget(self.ui.calendar_page)
+
+    def populate(self):
+        # Clear the list widgets
+        for list_widget in self.ui.list_widgets:
+            list_widget.clear()
+        # Clear the internal representation of the currently shown lessons
+        self.shown_lessons = [[] for i in range(7)]
+
+        next_week_start = self.week_monday + timedelta(weeks=1)
+
+        # Iterate over all the lessons
+        for lesson in self.all_lessons:
+            # Skip this lesson if it isn't part of the currently shown week
+            dt = datetime.fromtimestamp(lesson['dateTime'] // 1000)
+            if not self.week_monday <= dt <= next_week_start:
+                continue
+
+            # Calculate the end time of the lesson
+            end_dt = dt + timedelta(minutes=lesson["minutes"])
+
+            # Add the lesson to the list widget for the lesson's weekday
+            list_widget = self.ui.list_widgets[dt.weekday()]
+            list_widget.addItem(
+                f'{lesson["studentName"]}\n'
+                f'{dt:%I:%M %p} - {end_dt:%I:%M %p}\n'\
+                f'{lesson["location"]}'
+            )
+            # Add the lesson to the relevant internal list
+            self.shown_lessons[dt.weekday()].append(lesson)
+
+
+    def next_week(self):
+        self.week_monday += timedelta(weeks=1)
+        self.label_columns()
+        self.populate()
+
+    def previous_week(self):
+        self.week_monday -= timedelta(weeks=1)
+        self.label_columns()
+        self.populate()
+
+    def label_columns(self):
+        day = self.week_monday
+        self.ui.monday_label.setText(f'Monday\n{day:%B} {day.day}')
+        day += timedelta(days=1)
+        self.ui.tuesday_label.setText(f'Tuesday\n{day:%B} {day.day}')
+        day += timedelta(days=1)
+        self.ui.wednesday_label.setText(f'Wednesday\n{day:%B} {day.day}')
+        day += timedelta(days=1)
+        self.ui.thursday_label.setText(f'Thursday\n{day:%B} {day.day}')
+        day += timedelta(days=1)
+        self.ui.friday_label.setText(f'Friday\n{day:%B} {day.day}')
+        day += timedelta(days=1)
+        self.ui.saturday_label.setText(f'Saturday\n{day:%B} {day.day}')
+        day += timedelta(days=1)
+        self.ui.sunday_label.setText(f'Sunday\n{day:%B} {day.day}')
+
+    def show_details(self):
+        # Find the lesson's JSON in the internal list
+        sender = self.sender()
+        weekday = self.ui.list_widgets.index(sender)
+        index = sender.currentRow()
+        lesson = self.shown_lessons[weekday][index]
+
+        # Create and display a lesson info page for the selected lesson
+        self.ui.details_page = LessonInfoForm(self.ui.stacked_widget, lesson)
+        self.ui.stacked_widget.addWidget(self.ui.details_page)
+        self.ui.stacked_widget.setCurrentWidget(self.ui.details_page)
--- ui/calendar_ui.py
+++ ui/calendar_ui.py
@@ -32,6 +32,13 @@ class Ui_CalendarForm(object):
 
         self.horizontalLayout.addWidget(self.sidebar_frame)
 
+        self.stacked_widget = QStackedWidget(CalendarForm)
+        self.stacked_widget.setObjectName(u"stacked_widget")
+        self.calendar_page = QWidget()
+        self.calendar_page.setObjectName(u"calendar_page")
+        self.horizontalLayout_3 = QHBoxLayout(self.calendar_page)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(11, -1, -1, -1)
         self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.horizontalLayout_2 = QHBoxLayout()
@@ -40,15 +47,15 @@ class Ui_CalendarForm(object):
 
         self.horizontalLayout_2.addItem(self.horizontalSpacer)
 
-        self.next_button = QPushButton(CalendarForm)
-        self.next_button.setObjectName(u"next_button")
-
-        self.horizontalLayout_2.addWidget(self.next_button)
-
-        self.previous_button = QPushButton(CalendarForm)
+        self.previous_button = QPushButton(self.calendar_page)
         self.previous_button.setObjectName(u"previous_button")
 
         self.horizontalLayout_2.addWidget(self.previous_button)
+
+        self.next_button = QPushButton(self.calendar_page)
+        self.next_button.setObjectName(u"next_button")
+
+        self.horizontalLayout_2.addWidget(self.next_button)
 
 
         self.verticalLayout_2.addLayout(self.horizontalLayout_2)
@@ -56,78 +63,92 @@ class Ui_CalendarForm(object):
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setHorizontalSpacing(1)
-        self.wednesday_list = QListWidget(CalendarForm)
+        self.wednesday_list = QListWidget(self.calendar_page)
         self.wednesday_list.setObjectName(u"wednesday_list")
+        self.wednesday_list.setAlternatingRowColors(True)
+        self.wednesday_list.setWordWrap(True)
 
         self.gridLayout.addWidget(self.wednesday_list, 2, 2, 1, 1)
 
-        self.sunday_list = QListWidget(CalendarForm)
+        self.sunday_list = QListWidget(self.calendar_page)
         self.sunday_list.setObjectName(u"sunday_list")
+        self.sunday_list.setAlternatingRowColors(True)
+        self.sunday_list.setWordWrap(True)
 
         self.gridLayout.addWidget(self.sunday_list, 2, 6, 1, 1)
 
-        self.friday_list = QListWidget(CalendarForm)
+        self.friday_list = QListWidget(self.calendar_page)
         self.friday_list.setObjectName(u"friday_list")
+        self.friday_list.setAlternatingRowColors(True)
+        self.friday_list.setWordWrap(True)
 
         self.gridLayout.addWidget(self.friday_list, 2, 4, 1, 1)
 
-        self.thursday_list = QListWidget(CalendarForm)
+        self.thursday_list = QListWidget(self.calendar_page)
         self.thursday_list.setObjectName(u"thursday_list")
+        self.thursday_list.setAlternatingRowColors(True)
+        self.thursday_list.setWordWrap(True)
 
         self.gridLayout.addWidget(self.thursday_list, 2, 3, 1, 1)
 
-        self.tuesday_list = QListWidget(CalendarForm)
+        self.tuesday_list = QListWidget(self.calendar_page)
         self.tuesday_list.setObjectName(u"tuesday_list")
+        self.tuesday_list.setAlternatingRowColors(True)
+        self.tuesday_list.setWordWrap(True)
 
         self.gridLayout.addWidget(self.tuesday_list, 2, 1, 1, 1)
 
-        self.monday_list = QListWidget(CalendarForm)
+        self.monday_list = QListWidget(self.calendar_page)
         self.monday_list.setObjectName(u"monday_list")
+        self.monday_list.setAlternatingRowColors(True)
+        self.monday_list.setWordWrap(True)
 
         self.gridLayout.addWidget(self.monday_list, 2, 0, 1, 1)
 
-        self.saturday_list = QListWidget(CalendarForm)
+        self.saturday_list = QListWidget(self.calendar_page)
         self.saturday_list.setObjectName(u"saturday_list")
+        self.saturday_list.setAlternatingRowColors(True)
+        self.saturday_list.setWordWrap(True)
 
         self.gridLayout.addWidget(self.saturday_list, 2, 5, 1, 1)
 
-        self.monday_label = QLabel(CalendarForm)
+        self.monday_label = QLabel(self.calendar_page)
         self.monday_label.setObjectName(u"monday_label")
         self.monday_label.setAlignment(Qt.AlignCenter)
 
         self.gridLayout.addWidget(self.monday_label, 1, 0, 1, 1)
 
-        self.tuesday_label = QLabel(CalendarForm)
+        self.tuesday_label = QLabel(self.calendar_page)
         self.tuesday_label.setObjectName(u"tuesday_label")
         self.tuesday_label.setAlignment(Qt.AlignCenter)
 
         self.gridLayout.addWidget(self.tuesday_label, 1, 1, 1, 1)
 
-        self.saturday_label = QLabel(CalendarForm)
+        self.saturday_label = QLabel(self.calendar_page)
         self.saturday_label.setObjectName(u"saturday_label")
         self.saturday_label.setAlignment(Qt.AlignCenter)
 
         self.gridLayout.addWidget(self.saturday_label, 1, 5, 1, 1)
 
-        self.friday_label = QLabel(CalendarForm)
+        self.friday_label = QLabel(self.calendar_page)
         self.friday_label.setObjectName(u"friday_label")
         self.friday_label.setAlignment(Qt.AlignCenter)
 
         self.gridLayout.addWidget(self.friday_label, 1, 4, 1, 1)
 
-        self.thursday_label = QLabel(CalendarForm)
+        self.thursday_label = QLabel(self.calendar_page)
         self.thursday_label.setObjectName(u"thursday_label")
         self.thursday_label.setAlignment(Qt.AlignCenter)
 
         self.gridLayout.addWidget(self.thursday_label, 1, 3, 1, 1)
 
-        self.wednesday_label = QLabel(CalendarForm)
+        self.wednesday_label = QLabel(self.calendar_page)
         self.wednesday_label.setObjectName(u"wednesday_label")
         self.wednesday_label.setAlignment(Qt.AlignCenter)
 
         self.gridLayout.addWidget(self.wednesday_label, 1, 2, 1, 1)
 
-        self.sunday_label = QLabel(CalendarForm)
+        self.sunday_label = QLabel(self.calendar_page)
         self.sunday_label.setObjectName(u"sunday_label")
         self.sunday_label.setAlignment(Qt.AlignCenter)
 
@@ -137,7 +158,11 @@ class Ui_CalendarForm(object):
         self.verticalLayout_2.addLayout(self.gridLayout)
 
 
-        self.horizontalLayout.addLayout(self.verticalLayout_2)
+        self.horizontalLayout_3.addLayout(self.verticalLayout_2)
+
+        self.stacked_widget.addWidget(self.calendar_page)
+
+        self.horizontalLayout.addWidget(self.stacked_widget)
 
         self.horizontalLayout.setStretch(0, 1)
         self.horizontalLayout.setStretch(1, 4)
@@ -149,8 +174,8 @@ class Ui_CalendarForm(object):
 
     def retranslateUi(self, CalendarForm):
         CalendarForm.setWindowTitle(QCoreApplication.translate("CalendarForm", u"Calendar", None))
-        self.next_button.setText(QCoreApplication.translate("CalendarForm", u"Show Previous Week", None))
-        self.previous_button.setText(QCoreApplication.translate("CalendarForm", u"Show Next Week", None))
+        self.previous_button.setText(QCoreApplication.translate("CalendarForm", u"Show Previous Week", None))
+        self.next_button.setText(QCoreApplication.translate("CalendarForm", u"Show Next Week", None))
         self.monday_label.setText(QCoreApplication.translate("CalendarForm", u"Monday", None))
         self.tuesday_label.setText(QCoreApplication.translate("CalendarForm", u"Tuesday", None))
         self.saturday_label.setText(QCoreApplication.translate("CalendarForm", u"Saturday", None))
